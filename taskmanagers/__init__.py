9cefb1b5a1394a546eaa0e7661fafa7f0ce59523
Andrew croshaw <120042631+A-Croshaw@users.noreply.github.com>
2023-05-25 18:44:12 +0100

    Added Procfile and requirements.txt, Updated database details

--- taskmanagers/__init__.py
+++ taskmanagers/__init__.py
@@ -7,7 +7,14 @@ if os.path.exists("env.py"):
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["postgres://oxhgdmxx:Si6-gDcC_Y32rEKEcj_WsW_LkRrlAB8-@trumpet.db.elephantsql.com/oxhgdmxx"] = os.environ.get("DB_URL")
+else:
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+    app.config["postgres://oxhgdmxx:Si6-gDcC_Y32rEKEcj_WsW_LkRrlAB8-@trumpet.db.elephantsql.com/oxhgdmxx"] = uri
 
 db = SQLAlchemy(app)
 

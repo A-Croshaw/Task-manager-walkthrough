16f4f5436c7e13f16ac1ec2a86b9edef4273d15b
Andrew croshaw <120042631+A-Croshaw@users.noreply.github.com>
2023-05-25 18:48:49 +0100

    updated __init__.py

--- taskmanagers/__init__.py
+++ taskmanagers/__init__.py
@@ -9,12 +9,12 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["postgres://oxhgdmxx:Si6-gDcC_Y32rEKEcj_WsW_LkRrlAB8-@trumpet.db.elephantsql.com/oxhgdmxx"] = os.environ.get("DB_URL")
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["postgres://oxhgdmxx:Si6-gDcC_Y32rEKEcj_WsW_LkRrlAB8-@trumpet.db.elephantsql.com/oxhgdmxx"] = uri
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 db = SQLAlchemy(app)
 
